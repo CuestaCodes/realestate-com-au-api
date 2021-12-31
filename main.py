18eef439033ccd1f012f5ff610ee0c87707618b2
cuestacodes <cuestacodes@gmail.com>
2021-12-31 14:33:22 +1100

    Added main.py for ds project

---
+++ main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+	from realestate_com_au import RealestateComAu
+
+	api = RealestateComAu()
+
+	# Get property listings
+	listings = api.search(locations=["seventeen seventy, qld 4677"], channel="buy", keywords=["tenant"], exclude_keywords=["pool"])
+	print(listings)
