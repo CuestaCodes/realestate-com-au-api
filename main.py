a36c27e59d894727d7c21f8330318c8597d2f219
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-01-19 20:42:42 +1100

    altered to scrap VIC listings

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 if __name__ == '__main__':
     from realestate_com_au import RealestateComAu
+    import pandas as pd
 
     api = RealestateComAu()
 
     # Get property listings
-    listings = api.search(locations=["3205"], channel="buy", keywords=[
-                          "tenant"], exclude_keywords=["pool"])
-    print(listings)
+    listings = api.search(locations=["vic"], channel="buy")
+
+    df = pd.DataFrame(listings)
+
+    df.to_csv('realestate_listings.csv', index=False)
